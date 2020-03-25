d6be0a9f4ed52274343902758fd093c19643f332
ranraok <46853842+ranraok@users.noreply.github.com>
2020-03-25 18:06:47 -0400

    completed MVP"

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -19,6 +19,9 @@ function SavedList({ list }) {
       <div className="home-button">
         <Link to="/">Home</Link>
       </div>
+      <div className="home-button">
+        <Link to="/update-movie/:id">Update Movies</Link>
+      </div>
     </div>
   );
 }
